67aeb34adf08da18d305f0ab8ce33ac377f5b71d
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-02-24 19:26:57 +0000

    correct fs to fs promises and createst a test html

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
-const fs = require("fs");
+const fs = require('fs/promises');
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -17,12 +17,12 @@ const render = require("./src/page-template.js");
 let team = [];
 startProgram();
 async function startProgram() {
+    let newEngineer= new Engineer("Zan",1222,"csaba@test.com","CIMSGit")
 
-    team.push(new Manager("csaba",1222,"csaba@test.com",1222)
-    );
 
-    let htmlDoc = render(team);
-    await fs.writeFile(outputPath, htmlDoc);
+    team.push(newEngineer)
+
+    let htmlDoc = render(team)
+    await fs.writeFile(outputPath, htmlDoc)
 
 }
-// let newEngineer= new Engineer("csaba",1222,"csaba@test.com","CIMSGit")
