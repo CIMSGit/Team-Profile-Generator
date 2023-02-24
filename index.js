adebb2413d451c9f3780c468e6fad85f5f7452cd
CIMSGit <117454891+CIMSGit@users.noreply.github.com>
2023-02-24 21:41:07 +0000

    creates if statments and conditons prompts

--- index.js
+++ index.js
@@ -19,65 +19,83 @@ startProgram();
 
 
 async function startProgram() {
-    let {role,name,id,email,github,School,officeNumber,adding}= await inquirer
-    .prompt([    {
-        type: 'list',
-        name: 'role',
-        message: "What's your role?",
-        choices:["Manager","Intern","Engineer"]
-    },
-    {
+    let { name } = await inquirer
+    .prompt([{
         type: 'input',
         name: 'name',
-        message: "What's your name?",
-    }, {
+        message: "What's your name?"
+    },])
+    let { id } = await inquirer
+    .prompt([{
         type: 'input',
         name: 'id',
-        message: "Your ID?",
-    }, {
+        message: "What's your id?",
+    }])
+    let { email } = await inquirer
+    .prompt([{
         type: 'input',
         name: 'email',
-        message: "Email address?",
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: "Github?",
-    },
-    {
-        type: 'input',
-        name: 'School',
-        message: "School?",
-    },
-    
+        message: "What's your email?",
+    }])
+    let { role } = await inquirer
+        .prompt([{
+            type: 'list',
+            name: 'role',
+            message: "What's your role?",
+            choices: ["Manager", "Intern", "Engineer"]
+        },])
   
-    {
-        type: "list",
-        name:"adding",
-        message:"Do you want to someone else?",
-        choices:["Yes", "No"],
-    }
-    ]);
-    if( role === "Manager" ){
-        team.push( new Manager(name, id,email, officeNumber, ))
+  
+   
+
+    if (role === "Manager") {
+        let { officeNumber } = await inquirer
+        .prompt([{
+            type: 'input',
+            name: 'officeNumber',
+            message: "What's your office number?"
+        },])
+
+        team.push(new Manager(name, id, email, officeNumber,))
 
     }
-    if(role === "Intern"){
+    if (role === "Intern") {
+        let { School } = await inquirer
+        .prompt([{
+            type: 'input',
+            name: 'School',
+            message: "What's your School?"
+        },])
 
         team.push(new Intern(name, id, email, School));
+        
 
     }
-    if (role === "Engineer")
-    team.push(new Engineer(name, id,email, role, github));
-    if (team.push === true ){
+    if (role === "Engineer"){
+        let { github } = await inquirer
+        .prompt([{
+            type: 'input',
+            name: 'github',
+            message: "What's your github?"
+        }])
+        team.push(new Engineer(name, id, email, role, github));}
+    if (team.push === true) {
 
     }
+   
+    let { adding } = await inquirer
+        .prompt([{
+            type: "list",
+            name: "adding",
+            message: "Do you want to someone else?",
+            choices: ["Yes", "No"],
+        },])
 
-    
-    if (adding=== "Yes"){
+    if (adding === "Yes") {
         return startProgram()
     }
-    else{
-    let htmlDoc = render(team)
-    await fs.writeFile(outputPath, htmlDoc)}
+    else {
+        let htmlDoc = render(team)
+        await fs.writeFile(outputPath, htmlDoc)
+    }
 }
